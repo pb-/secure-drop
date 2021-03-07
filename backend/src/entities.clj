bb899d16a6d3d9daf34c4cb3ee2116837c150b7a
Paul Baecher <pbaecher@gmail.com>
2021-03-07 17:21:20 +0100

    Add basic filtering

--- backend/src/entities.clj
+++ backend/src/entities.clj
@@ -1,6 +1,8 @@
 (ns entities
   (:require [clojure.java.jdbc :as jdbc]
-            [ring.middleware.json :refer [wrap-json-response]]))
+            [ring.middleware.json :refer [wrap-json-response]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]))
 
 (defn ^:private load-entity [db id]
   (->> (jdbc/query db [(str "SELECT attribute, value "
@@ -9,17 +11,39 @@
        (map (fn [av] [(:attribute av) (:value av)]))
        (into {})))
 
-(defn ^:private load-entities [db]
-  (map
-    :entity
-    (jdbc/query db [(str "SELECT entity FROM datoms GROUP BY entity "
-                         "ORDER BY time DESC, rowid LIMIT 10")])))
+(defn ^:private load-entities [db conditions]
+  (let [[where-sql where-data] conditions]
+    (map
+      :entity
+      (jdbc/query db (concat
+                       [(str "SELECT entity FROM datoms "
+                             "WHERE true" where-sql " "
+                             "GROUP BY entity "
+                             "ORDER BY time DESC, rowid LIMIT 10")]
+                       where-data)))))
+
+(defn ^:private params->conditions [params]
+  (let [parts [(when-let [attribute (:a params)]
+                 [" AND attribute = ?", [attribute]])
+               (when-let [value (:v params)]
+                 [" AND value = ?", [value]])]
+        merge-fn (fn [merged part]
+                   (if part
+                     (let [[sql data] merged
+                           [s d] part]
+                       [(str sql s) (concat data d)])
+                     merged))]
+    (reduce merge-fn ["", []] parts)))
 
 (defn ^:private retreive' [request]
   (jdbc/with-db-transaction [t (:db request)]
     {:status 200
      :body {:entities
-            (for [entity (load-entities t)]
+            (for [entity (load-entities
+                           t (params->conditions (:params request)))]
               [entity (load-entity t entity)])}}))
 
-(def retrieve (wrap-json-response retreive'))
+(def retrieve (-> retreive'
+                  wrap-json-response
+                  wrap-keyword-params
+                  wrap-params))
